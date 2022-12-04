a6b5265495d1da301b750841794280fe479d24f6
gserifi <gent.serifi@gmail.com>
2022-12-04 14:57:06 +0100

    implemented first version of SEIZ-inspired model

--- model.py
+++ model.py
@@ -19,9 +19,10 @@ from networkgen import Network, NetworkType
 
 
 class Opinion(enum.IntEnum):
-  DISAGREE = 0
-  BELIEVE = 1
-  UNSURE = 2
+  SUSCEPTIBLE = 0
+  EXPOSED = 1
+  SKEPTIC = 2
+  INFECTED = 3
 
 
 class OpinionAgent(Agent):
@@ -30,8 +31,9 @@ class OpinionAgent(Agent):
   """
 
   class Params(NamedTuple):
-    initial_opinion: float  # TODO: docs
-    weight_decay: float  # TODO: docs
+    initial_opinion: Opinion  # TODO: docs
+    prob_S_with_I: float  # TODO: docs
+    prob_S_with_Z: float  # TODO: docs
 
   def __init__(self,
       unique_id: int,
@@ -47,83 +49,40 @@ class OpinionAgent(Agent):
 
     super().__init__(unique_id, model)
     self.params = params
-
-    #if self.params.initial_opinion is None:
-    self.state = np.clip(np.random.normal(self.params.initial_opinion, 0.1), 0, 1)  # TODO: parameterize this
-    #else:
-    #  self.state = self.params.initial_opinion
-
-  def compute_similarity(self, b):
-    return self._compute_similarity(self.state, b)
-
-  def _compute_similarity(self, a, b):
-    vecA = np.array([np.cos(a * np.pi / 2), np.sin(a * np.pi / 2)])
-    vecB = np.array([np.cos(b * np.pi / 2), np.sin(b * np.pi / 2)])
-    return np.square(np.dot(vecA, vecB))
-
-  def update_neighbor_weights(self):
-    neighbors = self.model.grid.get_neighbors(self.unique_id, include_center=False)
-    for neighbor_id in neighbors:
-      neighbor_agent: 'OpinionAgent' = self.model.schedule.agents[neighbor_id]
-      self.model.network.edges[neighbor_id, self.unique_id]['weight'] = 0.2 + 0.8 * self.state * self.compute_similarity(neighbor_agent.state)
+    self.state = self.params.initial_opinion
 
   def step(self) -> None:
     """@override(Agent)
     Defines behaviour within a single step
     """
 
+    # TODO: add docs explaining this
     neighbors = self.model.grid.get_neighbors(self.unique_id, include_center=False)
 
-    # Add a bit of noise
-    self.state = np.clip(np.random.normal(self.state, 0.1), 0, 1)  # TODO: make hyperparameter
-    self.update_neighbor_weights()
+    for neighbor_id in random.sample(neighbors, min(1, len(neighbors))):
+      neighbor = self.model.schedule.agents[neighbor_id]
 
-    sampled_neighbors = random.sample(neighbors, 3)
-    accumulated_step = 0.0
-    global_opinion = np.round(np.mean([agent.state for agent in self.model.grid.get_all_cell_contents()]))
-    for neighbor_id in sampled_neighbors:
-      neighbor_agent: 'OpinionAgent' = self.model.schedule.agents[neighbor_id]
+      if self.state == Opinion.EXPOSED:
+        transition_I = bernoulli.rvs(self.params.prob_S_with_I)
+        transition_Z = bernoulli.rvs(self.params.prob_S_with_Z)
 
-      effect = self.model.network.edges[neighbor_id, self.unique_id]['weight']
-      similarity = self.compute_similarity(neighbor_agent.state)
-      neighbor_opinion = np.round(neighbor_agent.state)
+        if transition_I and not transition_Z:
+          self.state = Opinion.INFECTED
 
-      if similarity > 0.25:
-        self.state = (self.state + neighbor_agent.state + neighbor_opinion * effect) / 3
+        if transition_Z and not transition_I:
+          self.state = Opinion.SKEPTIC
 
-      #global_effect = 1 - self._compute_similarity(global_opinion, 0.5)
-      #similarity = self.compute_similarity(neighbor_agent.state)
+      if (self.state, neighbor.state) == (Opinion.SUSCEPTIBLE, Opinion.INFECTED):
+        if bernoulli.rvs(self.params.prob_S_with_I):
+          self.state = Opinion.INFECTED
+        else:
+          self.state = Opinion.EXPOSED
 
-      #step = (neighbor_agent.state - self.state) * importance * 0.1  # TODO: make hyperparameter
-      #self.state += step
-      #neighbor_agent.state += step * similarity
-
-      #neighbor_opinion = np.round(neighbor_agent.state)
-      #step = (0.2 + 0.8 * self.compute_similarity(neighbor_opinion)) * (neighbor_opinion - self.state)
-      #accumulated_step += step * effect
-
-      #global_step = (0.05 + 0.05 * self.compute_similarity(global_opinion)) * (global_opinion - self.state)
-      #accumulated_step += global_step * global_effect
-
-      #diff = neighbor_agent.state - self.state
-
-      #scaling = neighbor_agent.state + self.state - abs(diff)
-
-      #diff *= self.model.network.edges[neighbor_id, self.unique_id]['weight']
-      #diff *= random.uniform(0, 1)
-
-      #self.state = min(max(self.state + diff, 0), 1)  # clip inside [0, 1]
-      #neighbor_agent.state = min(max(neighbor_agent.state + diff*0.25, 0), 1)  # clip inside [0, 1]
-
-      #scaling = self.model.network.edges[neighbor_id, self.unique_id]['weight']
-
-      #if self.state != neighbor_agent.state:
-        #if (self.state, neighbor_agent.state) in [(Opinion.DISAGREE, Opinion.BELIEVE), (Opinion.BELIEVE, Opinion.DISAGREE)]:
-        #  if bernoulli(scaling):
-        #    self.state = neighbor_agent.state
-        #elif self.state == Opinion.UNSURE:
-        #  if bernoulli(scaling):
-        #    self.state = neighbor_agent.state
+      if (self.state, neighbor.state) == (Opinion.SUSCEPTIBLE, Opinion.SKEPTIC):
+        if bernoulli.rvs(self.params.prob_S_with_Z):
+          self.state = Opinion.SKEPTIC
+        else:
+          self.state = Opinion.EXPOSED
 
 
 class OpinionModel(Model):
@@ -133,6 +92,7 @@ class OpinionModel(Model):
 
   def __init__(self,
       fraction_believers: float,
+      fraction_skeptics: float,
       agent_params: OpinionAgent.Params,
       network_type: NetworkType,
       network_params: Tuple
@@ -141,6 +101,7 @@ class OpinionModel(Model):
     Create a new model, initialize network and setup data collection
 
     :param fraction_believers: fraction of population that are initially believers
+    :param fraction_skeptics: fraction of population that are initially skeptics
     :param agent_params: parameters of agents
     :param network_type: type of network, see networkgen.py for more information
     :param network_params: parameters of network, depends on network type
@@ -148,13 +109,11 @@ class OpinionModel(Model):
 
     super().__init__()
 
+    # Setup Network
     self.fraction_believers = fraction_believers
-    self.network = Network.generate(network_type, network_params).to_directed()
+    self.fraction_skeptics = fraction_skeptics
+    self.network = Network.generate(network_type, network_params)
     self.population_size = len(self.network.nodes)
-
-    # Initialize Weights
-    for u, v in self.network.edges:
-      self.network.edges[u, v]['weight'] = 1.0
 
     self.grid = NetworkGrid(self.network)
 
@@ -168,15 +127,17 @@ class OpinionModel(Model):
       self.schedule.add(agent)
       self.grid.place_agent(agent, node)
 
-    for agent in self.grid.get_all_cell_contents():
-      agent.update_neighbor_weights()
-
-    # Initialize initial believers
+    # Initialize initial believers/skeptics
     n_initial_believers = math.floor(self.population_size * fraction_believers)
-    initial_believers: List[OpinionAgent] = random.sample(self.schedule.agents, n_initial_believers)
+    n_initial_skeptics = math.floor(self.population_size * fraction_skeptics)
 
-    for agent in initial_believers:
-      agent.state = np.random.normal(0.75, 0.05) #Opinion.BELIEVE
+    initial_biased: List[OpinionAgent] = random.sample(self.schedule.agents, n_initial_believers + n_initial_skeptics)
+
+    for i in range(n_initial_believers):
+      initial_biased[i].state = Opinion.INFECTED
+
+    for i in range(n_initial_believers, len(initial_biased)):
+      initial_biased[i].state = Opinion.SKEPTIC
 
     # Data Collector keeps track of agents' states
     self.data_collector = DataCollector(agent_reporters={"State": "state"})
